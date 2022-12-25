572db1e5f6b50490be2cf53b1dd3c2663a83545a
Hamdi Abdelatif <78198594+abdelatifhamdi702@users.noreply.github.com>
2022-12-25 15:45:35 +0100

    X_O game Java Swing

---
+++ src/x_o/X_O.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x_o;
+
+/**
+ *
+ * @author Abdelatif Hamdi
+ */
+public class X_O {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        (new Game()).setVisible(true);
+    }
+    
+}
